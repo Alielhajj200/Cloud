5af370e2c32f1cbe0b275ae707480c21ab5cfefb
Alielhajj200 <148614651+Alielhajj200@users.noreply.github.com>
2025-12-07 17:23:01 +0200

    connection to database

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
-const express = require("express")
-const app = express()
+import  express from "express";
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+
+dotenv.config();
+
+
+
+export const app = express()
 
 app.get('/',function(req,res){
     res.send("it worked updated");
 })
 
+const mongoURI = process.env.MONGO_URI || 'mongodb://localhost:27017/myDatabase';
 
-app.listen(process.env.PORT || 5000)
-module.exports = app
+mongoose.connect(mongoURI) // remove options
+  .then(() => console.log('MongoDB connected successfully'))
+  .catch(err => console.error('MongoDB connection error:', err));
+
+
+app.listen(process.env.PORT || 5000 , () => {
+    console.log('server running on port 5000')
+})
